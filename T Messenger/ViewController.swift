63ebbc9045bcbd53eb10118088700a8bd2e340ea
Andrey <onionschrod@gmail.com>
2020-02-14 09:10:43 +0300

    Initial Commit

---
+++ T Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  T Messenger
+//
+//  Created by Suspect on 14.02.2020.
+//  Copyright © 2020 Andrey Ivshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
